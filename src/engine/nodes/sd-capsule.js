4fefb043cc9b8fdb3027a45c583a590e0111ddc7
Szymon Kaliski <kaliskiszymon@gmail.com>
2017-01-25 19:50:36 +0100

    small node updates

--- src/engine/nodes/sd-capsule.js
+++ src/engine/nodes/sd-capsule.js
@@ -3,7 +3,7 @@ import generateNode from '../generate-node';
 export default generateNode({
   spec: {
     inlets: [
-      { id: 'p', type: 'vec3',   value: 'vec3(0.0)'            },
+      { id: 'p', type: 'vec3',   value: 'p'                    },
       { id: 'a', type: 'vec3',   value: 'vec3(0.0, 0.0, -0.2)' },
       { id: 'b', type: 'vec3',   value: 'vec3(0.0, 0.0, 0.2)'  },
       { id: 'r', type: 'float3', value: '0.5'                  },
--- src/engine/nodes/ui-pack3.js
+++ src/engine/nodes/ui-pack3.js
@@ -3,9 +3,9 @@ import generateNode from '../generate-node';
 export default generateNode({
   spec: {
     inlets: [
-      { id: 'f1', type: 'float', 'value': '0.0' },
-      { id: 'f2', type: 'float', 'value': '0.0' },
-      { id: 'f3', type: 'float', 'value': '0.0' }
+      { id: 'x', type: 'float', 'value': '0.0' },
+      { id: 'y', type: 'float', 'value': '0.0' },
+      { id: 'z', type: 'float', 'value': '0.0' }
     ],
     outlet: { id: 'v', type: 'vec3' }
   },
