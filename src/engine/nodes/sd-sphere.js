2c4cdf43eb171ea80500ac44cf159fbd9999e615
Szymon Kaliski <kaliskiszymon@gmail.com>
2017-01-28 21:24:06 +0100

    more connections but easier to understand - every node needs sysInput

--- src/engine/nodes/sd-sphere.js
+++ src/engine/nodes/sd-sphere.js
@@ -3,8 +3,8 @@ import generateNode from '../generate-node';
 export default generateNode({
   spec: {
     inlets: [
-      { id: 'p', type: 'vec3',  value: 'p'   },
-      { id: 'r', type: 'float', value: '0.5' }
+      { id: 'p', type: 'vec3',  value: 'vec3(0.0)' },
+      { id: 'r', type: 'float', value: '0.5'       }
     ],
     outlet: { id: 'd', type: 'float' }
   },
